2e1a779730501793ada597ae2d66edb1110055df
Bakann Dy <bakann.dy@keyrus.com>
2023-05-01 17:43:37 +0200

    RSVP

---
+++ public/blog/index.js
@@ -0,0 +1,59 @@
+// Import stylesheets
+import './style.css';
+// Firebase App (the core Firebase SDK) is always required
+import { initializeApp } from 'firebase/app';
+
+// Add the Firebase products and methods that you want to use
+import {} from 'firebase/auth';
+import {} from 'firebase/firestore';
+
+import * as firebaseui from 'firebaseui';
+
+// Document elements
+const startRsvpButton = document.getElementById('startRsvp');
+const guestbookContainer = document.getElementById('guestbook-container');
+
+const form = document.getElementById('leave-message');
+const input = document.getElementById('message');
+const guestbook = document.getElementById('guestbook');
+const numberAttending = document.getElementById('number-attending');
+const rsvpYes = document.getElementById('rsvp-yes');
+const rsvpNo = document.getElementById('rsvp-no');
+
+let rsvpListener = null;
+let guestbookListener = null;
+
+let db, auth;
+
+async function main() {
+  const firebaseConfig = {
+  apiKey: "AIzaSyA1UM7o78Yd9dBI_TKiS3PZIo4MDYXpsn0",
+  authDomain: "prenoms-en-france.firebaseapp.com",
+  projectId: "prenoms-en-france",
+  storageBucket: "prenoms-en-france.appspot.com",
+  messagingSenderId: "336169393623",
+  appId: "1:336169393623:web:0a59b39d77df26db90ff92",
+  measurementId: "G-SCQEE7DF2X"
+};
+`
+   initializeApp(firebaseConfig);
+
+  // FirebaseUI config
+  const uiConfig = {
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInOptions: [
+      // Email / Password Provider.
+      EmailAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+        // Handle sign-in.
+        // Return false to avoid redirect.
+        return false;
+      },
+    },
+  };
+
+  // const ui = new firebaseui.auth.AuthUI(auth);
+}
+main();
